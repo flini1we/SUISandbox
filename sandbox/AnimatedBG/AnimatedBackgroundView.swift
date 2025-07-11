43b10f31058faba153103f427f1d1cd61273e06d
setuper <setuper@MacBookPro.home>
2025-07-11 12:51:06 +0300

    animated bg view

---
+++ sandbox/AnimatedBG/AnimatedBackgroundView.swift
@@ -0,0 +1,125 @@
+import SwiftUI
+
+struct AnimatedBackgroundView: View {
+    @State private var offsets: [CGSize] = .init(repeating: .zero, count: 3)
+    
+    private let colorOpacity = 0.8
+    
+    var body: some View {
+        GeometryReader { proxy in
+            let width = proxy.size.width * 0.7
+            ZStack {
+                Circle()
+                    .fill(.cyan.opacity(colorOpacity))
+                    .offset(offsets[0])
+                    .frame(width: width)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.yellow.opacity(colorOpacity))
+                    .offset(offsets[1])
+                    .frame(width: width)
+                    .blendMode(.screen)
+                
+                Circle()
+                    .fill(.pink.opacity(colorOpacity))
+                    .offset(offsets[2])
+                    .frame(width: width)
+                    .blendMode(.screen)
+            }
+            .position(
+                x: proxy.size.width / 2,
+                y: proxy.size.height / 2
+            )
+            .task {
+                for index in offsets.indices {
+                    animateCircles(at: index, in: proxy.size)
+                }
+            }
+        }
+    }
+    
+    private func animateCircles(at index: Int, in size: CGSize, movingSpeed speed: CGFloat = 20) {
+        let destination: CGSize = .randomEdgePoint(
+            maxX: size.height * 0.4,
+            maxY: size.height * 0.4
+        )
+        let current = offsets[index]
+        let distance = hypot(
+            destination.width - current.width,
+            destination.height - current.height
+        )
+        let travelTime = Double(distance / speed)
+        
+        withAnimation(.linear(duration: travelTime)) {
+            offsets[index] = destination
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + travelTime) {
+            animateCircles(at: index, in: size)
+        }
+    }
+}
+
+struct CardView: View {
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .topLeading) {
+                
+                AnimatedBackgroundView()
+                    .ignoresSafeArea()
+                
+                RoundedRectangle(cornerRadius: 32, style: .continuous)
+                    .fill(.ultraThinMaterial)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 32, style: .continuous)
+                            .stroke(Color.white.opacity(0.3), lineWidth: 2)
+                    )
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack(alignment: .center) {
+                        Text("Welcome, Daniel")
+                            .font(Font.system(.title).smallCaps())
+                            .bold()
+                    }
+                    
+                    Text("A member of your household has perhaps been withholding some negative emotions for a long time. He or she may finally let loose with everything today. This could prove disconcerting, Capricorn, as you probably had no idea this was going on. Some honest talk is definitely in order. Try to show that you understand. All should be fine in the end.")
+                        .font(.callout.smallCaps())
+                        .monospaced()
+                    
+                }
+                .padding()
+                
+            }
+            .frame(maxWidth: .infinity)
+            .clipShape(RoundedRectangle(cornerRadius: 32, style: .continuous))
+        }
+    }
+}
+
+private extension CGSize {
+    
+    static func randomEdgePoint(maxX: CGFloat, maxY: CGFloat) -> Self {
+        switch Int.random(in: 0..<4) {
+        case 0:
+            return .init(width: .random(in: -maxX...maxX), height: -maxY)
+        case 1:
+            return .init(width: .random(in: -maxX...maxX), height:  maxY)
+        case 2:
+            return .init(width: -maxX, height: .random(in: -maxY...maxY))
+        default:
+            return .init(width:  maxX, height: .random(in: -maxY...maxY))
+        }
+    }
+}
+
+#Preview {
+    ZStack(alignment: .center) {
+        Color.black
+            .ignoresSafeArea()
+        
+        CardView()
+            .frame(width: .infinity, height: UIScreen.main.bounds.height / 3)
+    }
+}
